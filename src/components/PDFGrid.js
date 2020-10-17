fd205a2b151c8ec69d4dd401bc48d14a24c58f1e
meep-morp <gracieaerogirl@gmail.com>
2020-10-16 21:45:10 -0500

    Finished Styles and final touches

--- src/components/PDFGrid.js
+++ src/components/PDFGrid.js
@@ -8,10 +8,11 @@ const PDFGrid = ({ list }) => {
 
 	return (
 		<div className="grid">
-			{list.length === 0 &&
-				<div className="center output">
+			{list.length === 0 && (
+				<div style={{ height: "40vh" }} className="center output">
 					<h2>Upload a PDF file to view it's text contents.</h2>
-				</div>}
+				</div>
+			)}
 			{list.map(item => (
 				<PDFDisplay data={item} />
 			))}
